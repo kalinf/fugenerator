aa60035a0b34a31dcc39cf301877dfac05ebe910
kalina <kalinafilus@gmail.com>
2025-06-05 11:44:21 +0200

    transformations

--- fugenerator.py
+++ fugenerator.py
@@ -1,4 +1,5 @@
 import music21 as music
+import copy
 
 music.environment.set('midiPath', '/usr/bin/timidity')
 
@@ -13,15 +14,24 @@ def flatten(lst):
         res += elem
     return res
 
-def notes_from_melody(melody):
-    res = []
-    for measure in melody:
-        res += measure.notesAndRests.stream()
-    return res
+def tempo_change(melody, k):
+    notes = notes_of_melody(melody)
+    for note in notes:
+        note.duration = music.duration.Duration(note.duration.quarterLength * k)
+    return melody_of_notes(notes)
+
+def notes_of_melody(melody):
+    return list(melody.notesAndRests)
+
+def melody_of_notes(notes):
+    melody = music.stream.Stream()
+    for note in notes:
+        melody.append(note)
+    return melody
 
 def inversion(melody):
     inverted_melody = music.stream.Stream()
-    notes = notes_from_melody(melody)
+    notes = notes_of_melody(melody)
     for i, note in enumerate(notes):
         if i == 0: 
             inverted_melody.append(note)
@@ -34,10 +44,60 @@ def inversion(melody):
 
     return inverted_melody
 
+def transposition(melody, interval):
+    transposed_melody = music.stream.Stream()
+    notes = notes_of_melody(melody)
+    for note in notes:
+        transposed_melody.append(note.transpose(interval))
+    return transposed_melody
+
+def retrogradation(melody):
+    notes = notes_of_melody(melody)
+    notes.reverse()
+    return melody_of_notes(notes)
+
+def augmentation(melody, k):
+    return tempo_change(melody, k)
+    
+def diminution(melody, k):
+    return tempo_change(melody, 1/k)
+
+# transformations - list of unary transforming funcktions
+def generate_variants(melody, transformations, iterations):
+    variants = [melody]
+    old_variants = variants
+    for i in range(iterations):
+        new_variants = []
+        for transformation in transformations:
+            new_variants += [transformation(m) for m in old_variants]
+        variants += new_variants # chyba chcę usuwać powtórzenia
+        old_variants = new_variants
+    return variants         
+        
+def instantiate_binary_transformation(transformation, k):
+    return lambda m: transformation(m, k)
+
+def get_transformations(unary_transformations, binary_transformations):
+    transformations = copy.deepcopy(unary_transformations)
+    for transformation in binary_transformations:
+        for j in range(transformation[1], transformation[2]):
+            transformations.append(instantiate_binary_transformation(transformation[0], j))
+    return transformations
+        
+UNARY_TRANSFORMATIONS = [inversion, retrogradation]
+BINARY_TRANSFORMATIONS = [(transposition, -12, 13),
+                          (augmentation, 2, 5), 
+                          (diminution, 2, 9)]
+
 def main():
     kurki = make_kurki_trzy()
-    inverted_kurki = inversion(kurki)
-    inverted_kurki.show('midi')
-    # inverted_kurki.show()
+    # new_kurki = transposition(kurki, 2)
+    # new_kurki.show('midi')
+    # new_kurki.show()
+    transformations = get_transformations(UNARY_TRANSFORMATIONS, BINARY_TRANSFORMATIONS)
+    variants = generate_variants(kurki, transformations, 3)
+    print(len(variants))
+    # for variant in variants:
+    #     print(variant)
 
 main()
