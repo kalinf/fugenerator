89f8277f54049887b80dede1613c46bd82143a93
kalina <kalinafilus@gmail.com>
2025-06-06 11:18:33 +0200

    domain generation (bug)

--- fugenerator.py
+++ fugenerator.py
@@ -1,5 +1,6 @@
 import music21 as music
 import copy
+import itertools
 
 music.environment.set('midiPath', '/usr/bin/timidity')
 
@@ -27,6 +28,12 @@ def melody_of_notes(notes):
     melody = music.stream.Stream()
     for note in notes:
         melody.append(note)
+    return melody
+
+def merge_melodies(melodies):
+    melody = music.stream.Stream()
+    for m in melodies:
+        melody += m
     return melody
 
 def inversion(melody):
@@ -64,13 +71,19 @@ def diminution(melody, k):
 
 # transformations - list of unary transforming funcktions
 def generate_variants(melody, transformations, iterations):
-    variants = [melody]
+    variants = set()
+    variants.add(melody)
     old_variants = variants
-    for i in range(iterations):
+    for _ in range(iterations):
         new_variants = []
-        for transformation in transformations:
-            new_variants += [transformation(m) for m in old_variants]
-        variants += new_variants # chyba chcę usuwać powtórzenia
+        # for transformation in transformations:
+        #     # new_variants += [transformation(m) for m in old_variants]
+        #     for m in old_variants:
+        #         print('base')
+        #         m.show('text')
+        #         print('transformed')
+        #         transformation(m).show('text')
+        variants |= set(new_variants)
         old_variants = new_variants
     return variants         
         
@@ -89,15 +102,42 @@ BINARY_TRANSFORMATIONS = [(transposition, -12, 13),
                           (augmentation, 2, 5), 
                           (diminution, 2, 9)]
 
+# r == length // len(melody)
+def gen_domain(variants, r):
+    domain = []
+    domain += list(itertools.combinations(variants, r))
+    return domain
+
 def main():
     kurki = make_kurki_trzy()
+    print('inwersja')
+    inversion(kurki).show('text')
+    
+    inverted_kurki = inversion(kurki)
+    inverted_kurki.show()
+    # print('transpozycja')
     # new_kurki = transposition(kurki, 2)
-    # new_kurki.show('midi')
-    # new_kurki.show()
-    transformations = get_transformations(UNARY_TRANSFORMATIONS, BINARY_TRANSFORMATIONS)
-    variants = generate_variants(kurki, transformations, 3)
-    print(len(variants))
-    # for variant in variants:
-    #     print(variant)
+    # new_kurki.show('text')
+    # # new_kurki.show()
+    # transformations = get_transformations(UNARY_TRANSFORMATIONS, BINARY_TRANSFORMATIONS)
+    # print(transformations)
+    # variants = generate_variants(kurki, transformations, 1)
+    # print(len(variants))
+    # variant = next(iter(variants))
+    # print('variants:')
+    # for i, variant in enumerate(variants):
+    #     print(i)
+    #     variant.show('text')
+    #     print()
+        
+    # domain = gen_domain(variants, 8)
+    # print('number of combinations:')
+    # print(len(domain))
+    # print('first domain element:')
+    # domain[0][0].show('text')
+    # melody = merge_melodies(domain[0])
+    # # melody.show('midi')
+    # print('domain merged to melody:')
+    # melody.show('text')
 
 main()
